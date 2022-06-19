a8dcc6d277b56bc49056a5dad993661f820a9b76
Wael Latif <waellatif88@gmail.com>
2022-06-19 10:49:13 +0200

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  static String id = 'LoginScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+static String id = 'RegistrationScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
